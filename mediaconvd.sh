51009943551fb1f3712a8e55e7cece667daad132
TOGURO Mikito <mit@shalab.net>
2021-05-05 03:47:47 +0900

    書き出し先のファイルが存在していると、権限が異なるのでmvに失敗する問題があったので、既に書き出し先のファイルがある場合には処理をスキップするように修正

--- mediaconvd.sh
+++ mediaconvd.sh
@@ -14,62 +14,74 @@ inotifywait -m --event close_write,moved_to "${HOTPATH}" 2> /dev/null | \
 		    case "$(echo "${filename##*.}" | tr '[:upper:]' '[:lower:]')" in
 			"heic")
 			    # srcファイルの所持者の権限で実行する
-			    declare msg=$(sudo -n -u $(stat -c '%U' "${dirpath}${filename}") mogrify -format jpg "${dirpath}${filename}")
-			    if [ ! -z "${msg}" ] ; then
-				echo "${msg}" > "${dirpath}${filename%.*}.log"
+			    if [ ! -f "${dirpath}${filename%.*}.jpg" ] ; then
+				declare msg=$(sudo -n -u $(stat -c '%U' "${dirpath}${filename}") mogrify -format jpg "${dirpath}${filename}")
+				if [ ! -z "${msg}" ] ; then
+				    echo "${msg}" > "${dirpath}${filename%.*}.log"
+				fi
+			    else
+				echo "${dirpath}${filename%.*}.jpg" is existed, skip the file.
 			    fi
 			    ;;
 			"mov")
-			    touch "${dirpath}CONVERT-GO-AHEAD"
-			    declare msg="$(nice ffmpeg -loglevel warning -i "${dirpath}$filename" -pix_fmt yuv420p "${dirpath}${filename%.*}-conv.mp4" 2>&1)"
-			    if [ ! -z "${msg}" ] ; then
-				echo "${msg}" > "${dirpath}${filename%.*}.log"
-			    fi
-			    if [ -f "${dirpath}${filename%.*}-conv.mp4" ] ; then
-				mv "${dirpath}${filename%.*}-conv.mp4" "${dirpath}${filename%.*}.mp4"
-			    fi
-			    if [ $(id -u) -eq 0 ] ; then
-				if [ -f "${dirpath}${filename%.*}.log" ] ; then
-				    chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.log"
+			    if [ ! -f "${dirpath}${filename%.*}.mp4" ] ; then
+				touch "${dirpath}CONVERT-GO-AHEAD.log"
+				declare msg="$(nice ffmpeg -loglevel warning -i "${dirpath}$filename" -pix_fmt yuv420p "${dirpath}${filename%.*}-conv.mp4" 2>&1)"
+				if [ ! -z "${msg}" ] ; then
+				    echo "${msg}" > "${dirpath}${filename%.*}.log"
 				fi
-				if [ -f "${dirpath}${filename%.*}.mp4" ] ; then
-				    chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.mp4"
+				if [ -f "${dirpath}${filename%.*}-conv.mp4" ] ; then
+				    mv "${dirpath}${filename%.*}-conv.mp4" "${dirpath}${filename%.*}.mp4"
 				fi
+				if [ $(id -u) -eq 0 ] ; then
+				    if [ -f "${dirpath}${filename%.*}.log" ] ; then
+					chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.log"
+				    fi
+				    if [ -f "${dirpath}${filename%.*}.mp4" ] ; then
+					chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.mp4"
+				    fi
+				else
+				    if [ -f "${dirpath}${filename%.*}.log" ] ; then
+					sudo chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.log"
+				    fi
+				    if [ -f "${dirpath}${filename%.*}.mp4" ] ; then
+					sudo chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.mp4"
+				    fi
+				fi
+				rm "${dirpath}CONVERT-GO-AHEAD.log"
 			    else
-				if [ -f "${dirpath}${filename%.*}.log" ] ; then
-				    sudo chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.log"
-				fi
-				if [ -f "${dirpath}${filename%.*}.mp4" ] ; then
-				    sudo chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.mp4"
-				fi
+				echo "${dirpath}${filename%.*}.mp4" is existed, skip the file.
 			    fi
-			    rm "${dirpath}CONVERT-GO-AHEAD"
 			    ;;
 			"mkv")
-			    touch "${dirpath}CONVERT-GO-AHEAD"
-			    declare msg="$(nice ffmpeg -loglevel warning -i "${dirpath}$filename" -vcodec copy "${dirpath}${filename%.*}-conv.mp4" 2>&1)"
-			    if [ ! -z "${msg}" ] ; then
-				echo "${msg}" > "${dirpath}${filename%.*}.log"
-			    fi
-			    if [ -f "${dirpath}${filename%.*}-conv.mp4" ] ; then
-				mv "${dirpath}${filename%.*}-conv.mp4" "${dirpath}${filename%.*}.mp4"
-			    fi
-			    if [ $(id -u) -eq 0 ] ; then
-				if [ -f "${dirpath}${filename%.*}.log" ] ; then
-				    chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.log"
+			    if [ ! -f "${dirpath}${filename%.*}.mp4" ] ; then
+				touch "${dirpath}CONVERT-GO-AHEAD.log"
+				declare msg="$(nice ffmpeg -loglevel warning -i "${dirpath}$filename" -vcodec copy "${dirpath}${filename%.*}-conv.mp4" 2>&1)"
+				if [ ! -z "${msg}" ] ; then
+				    echo "${msg}" > "${dirpath}${filename%.*}.log"
 				fi
-				if [ -f "${dirpath}${filename%.*}.mp4" ] ; then
-				    chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.mp4"
+				if [ -f "${dirpath}${filename%.*}-conv.mp4" ] ; then
+				    mv "${dirpath}${filename%.*}-conv.mp4" "${dirpath}${filename%.*}.mp4"
 				fi
+				if [ $(id -u) -eq 0 ] ; then
+				    if [ -f "${dirpath}${filename%.*}.log" ] ; then
+					chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.log"
+				    fi
+				    if [ -f "${dirpath}${filename%.*}.mp4" ] ; then
+					chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.mp4"
+				    fi
+				else
+				    if [ -f "${dirpath}${filename%.*}.log" ] ; then
+					sudo chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.log"
+				    fi
+				    if [ -f "${dirpath}${filename%.*}.mp4" ] ; then
+					sudo chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.mp4"
+				    fi
+				fi
+				rm "${dirpath}CONVERT-GO-AHEAD.log"
 			    else
-				if [ -f "${dirpath}${filename%.*}.log" ] ; then
-				    sudo chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.log"
-				fi
-				if [ -f "${dirpath}${filename%.*}.mp4" ] ; then
-				    sudo chown $(stat -c '%U:%G' "${dirpath}${filename}") "${dirpath}${filename%.*}.mp4"
-				fi
+				echo "${dirpath}${filename%.*}.mp4" is existed, skip the file.
 			    fi
-			    rm "${dirpath}CONVERT-GO-AHEAD"
 			    ;;
 			"mp4"|"jpg"|"log")
 			    ;;
